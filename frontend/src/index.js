4b25c9dea9823be6913ae8c3b53d67d11282d6b0
SlimHong <807672412@qq.com>
2018-05-04 20:05:23 +0800

    修改IE兼容性

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,9 @@
+// import "babel-polyfill";
+import "core-js/es6";
+
+import "media-match";
+import "raf/polyfill";
+
 import React from 'react';
 import { render } from "react-dom";
 import App from "./containers/App";
